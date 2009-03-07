3fc1fe52a4547f352b24a5f5466fa649f6ff8e4a
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2009-03-07 02:21:16 -0800

    Add actual functionality. These commit messages could probably be smarter.

--- PvPTimer.lua
+++ PvPTimer.lua
@@ -1,6 +1,12 @@
 local UPDATE_PERIOD = 0.5
 local elapsed = 0.5
 
+local str_format = string.format
+
+local PVP_MAX_TIME = 301000
+local PVP_ICON_FFA = 'Interface\\TargetingFrame\\UI-PVP-FFA'
+local PVP_ICON = 'Interface\\TargetingFrame\\UI-PVP-'
+
 local f = CreateFrame("frame")
 local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject(
 	'PvPTimer', 
@@ -11,6 +17,21 @@ f:SetScript("OnUpdate", function(self, elap)
 	elapsed = elapsed + elap
 	if elapsed < UPDATE_PERIOD then return end
 	elapsed = 0
-		
-	dataobj.text = ''
+	
+	local time = GetPVPTimer()
+	local text = ''
+	local ffa = UnitIsPVPFreeForAll("player")
+	
+	if( time < PVP_MAX_TIME ) then
+		local min = floor(time/60000)
+		local sec = floor(time/1000)%60
+		sec = sec < 10 and '0'..sec or sec
+		text = str_format("|cffffffff%s%s:%s|r", ffa and 'FFA' or '', min, sec)
+	elseif( UnitIsPVP("player") ) then
+		text = ffa and '|cffff1919FFA|r' or '|cffff1919PvP|r'
+	else
+		text = '|cff8c8c8cPvE|r'
+	end
+	
+	dataobj.text = text
 end)
