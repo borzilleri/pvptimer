8a3cb5a46371083da92405ef9abda40ea01f0ae3
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2009-03-07 01:13:34 -0800

    Added basic functionality.

--- PvPTimer.lua
+++ PvPTimer.lua
@@ -1,1 +1,16 @@
+local UPDATE_PERIOD = 0.5
+local elapsed = 0.5
 
+local f = CreateFrame("frame")
+local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject(
+	'PvPTimer', 
+	{type='data source', text='', label='PvP: '}
+)
+
+f:SetScript("OnUpdate", function(self, elap)
+	elapsed = elapsed + elap
+	if elapsed < UPDATE_PERIOD then return end
+	elapsed = 0
+		
+	dataobj.text = ''
+end)
